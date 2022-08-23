5baba87368b795021d928afc7814270eb18ab555
MunYongJang <dltk456@naver.com>
2022-08-23 23:08:29 +0900

    showPopup

--- lib/listview.dart
+++ lib/listview.dart
@@ -47,6 +47,34 @@ class _ListViewPageState extends State<ListViewPage> {
     '10. There are different types of careers you can pursue in your life. Which one will it be?',
   ];
 
+  void showPopup(context, title, image, description) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: 380,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(image, width: 200, height: 200),
+                  ), // circleAvatar 위젯이랑 비슷하고 이미지를 사각형으로 출력해줌
+                  const SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
+            ),
+          ); // 우리가 원하는 팝업창 구성
+        }); // 플러터 제공 메소드
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width * 0.6;
@@ -62,37 +90,49 @@ class _ListViewPageState extends State<ListViewPage> {
       body: ListView.builder(
         itemCount: titleList.length,
         itemBuilder: (context, index) {
-          return Card(
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset(imageList[index]),
-                  // itemCount 의 개수만큼 카드가 생성, 각 카드 인덱스에 맞는 이미지가 하나씩 전달됨
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Text(
-                        titleList[index],
-                        style: const TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      SizedBox(
-                        width: width,
-                        child: Text(description[index]),
-                      )
-                    ],
+          return GestureDetector(
+            // 각 리스트 아이템 터치하면 기능
+            onTap: () {
+              debugPrint(titleList[index]);
+              showPopup(context, titleList[index], imageList[index],
+                  description[index]);
+            },
+            child: Card(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(imageList[index]),
+                    // itemCount 의 개수만큼 카드가 생성, 각 카드 인덱스에 맞는 이미지가 하나씩 전달됨
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          titleList[index],
+                          style: const TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          width: width,
+                          child: Text(
+                            description[index],
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[500]),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           );
         },
