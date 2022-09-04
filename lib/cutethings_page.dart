acbe97dcbea8268d38c047763470f938437900df
MunYongJang <dltk456@naver.com>
2022-09-04 15:17:04 +0900

    Add like button

--- lib/cutethings_page.dart
+++ lib/cutethings_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
 
 import 'model.dart';
 
-class CuteThingsPage extends StatelessWidget {
+class CuteThingsPage extends StatefulWidget {
   const CuteThingsPage({Key? key, required this.cuteThings}) : super(key: key);
 
   // 생성자에 추가해 준다. 널 값을 가질 수 없기 때문에 required 붙임
@@ -10,25 +11,43 @@ class CuteThingsPage extends StatelessWidget {
   final CuteThings cuteThings;
 
   @override
+  State<CuteThingsPage> createState() => _CuteThingsPageState();
+}
+
+class _CuteThingsPageState extends State<CuteThingsPage> {
+
+  bool isLiked = false;
+  int likeCount = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(cuteThings.name),
+        title: Text(widget.cuteThings.name),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
                 height: 200,
                 width: 200,
-                child: Image.asset(cuteThings.imgPath)),
+                child: Image.asset(widget.cuteThings.imgPath)),
             const SizedBox(
               height: 20,
             ),
-            Text('It lives in ' + cuteThings.location,
+            Text('It lives in ' + widget.cuteThings.location,
               style: const TextStyle(
                 fontSize: 18
               ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            LikeButton(
+              size: 30,
+              isLiked: isLiked,
+              likeCount: likeCount,
             )
           ],
         ),
